445c94af1e505ca7751ec88559dbba4c3ad21ebe
hamasaki <hamasaki0011@me.com>
2023-07-26 16:50:38 +0900

    Make a reduce arrangement of source code

--- app_old/accounts/templatetags/accounts_user.py
+++
@@ -1,9 +0,0 @@
-from django import template
-register = template.Library()
-
-@register.filter
-def user_display(user):
-    user_display = "Guest"
-    if user.is_authenticated:
-        user_display = user.email
-    return user_display
--- app_old/accounts/urls.py
+++
@@ -1,6 +0,0 @@
-from .views import ProfileUpdateView
-from django.urls import path
-
-urlpatterns = [
-    path("<int:pk>/", ProfileUpdateView.as_view(), name="profile-update"),
-]
